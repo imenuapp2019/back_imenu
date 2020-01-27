2b46508b55703b6f26dc9b0057399338ff5317c9
Juanma <Hernaiz>
2020-01-27 20:45:25 +0100

    [JM] [panel de administrador]
    Formulario del restaurante

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Validator;
+use Auth;
+
+class AdminController extends Controller
+{
+
+}
---
+++ app/Http/Controllers/ControllerFormularioResrtaurante.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControllerFormularioResrtaurante extends Controller
+{
+   public function callController() {
+
+    return view('Restaurante/formularioMakeRestaurante');
+
+
+   }
+}
