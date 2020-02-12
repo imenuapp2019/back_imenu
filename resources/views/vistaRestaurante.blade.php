2c141c32f6dba564d70bb30e44699178075fb507
Juanma <Hernaiz>
2020-02-12 18:16:44 +0100

    [JM] (plato, vistas)
    crud de plato, alergenos y fotos del plato. Vista de restaurantes y usuarios en panel de administrador

--- resources/views/vistaRestaurante.blade.php
+++ resources/views/vistaRestaurante.blade.php
@@ -6,12 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <link rel="stylesheet" href=" {{asset('css/fondo.css')}}" type="text/css">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <title>Restaurante</title>
 </head>
-<body>
+<body class= "fondo">
 <div class = 'container'>
-    <p class="h1 text-center"  >{{$restaurante->name}} </p>
+    <p class="h1 text-center text-white"  >{{$restaurante->name}} </p>
 </div>
 
     <div class = 'container'>
